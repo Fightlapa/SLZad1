ff92e364de9f4d3ad25700453ec2d4de4f97b450
Jablonski, Jakub <jakub.jablonski@intel.com>
2019-03-07 08:35:49 +0100

    Final version

--- JezykiSkryptowe1/JezykiSkryptowe1.py
+++ JezykiSkryptowe1/JezykiSkryptowe1.py
@@ -6,7 +6,7 @@ EXTREME_VALUE = 0xfffffffff
 
 def generate_test_data():
     with open("TestData.txt", "w") as fout:
-        for i in range (-SIZE - 0x1, SIZE + 0x1):
+        for i in range (-SIZE, SIZE):
             fout.write(f"{i},{oct(i)},")
         extreme_value = 0xfffffffff
         fout.write(f"{EXTREME_VALUE},{oct(EXTREME_VALUE)}")
@@ -14,14 +14,18 @@ def generate_test_data():
 
 def measure(func):
     empty_loop_start = time.clock()
-    for i in range (-SIZE - 0x1, SIZE + 0x1):
-        this_is_to_avoid_negative_time = "Problem exists probably because of the cycles required to perform both loops and it occurs, that empty one executes slower."
+    for i in range (-SIZE, SIZE):
+        this_is_to_avoid_negative_time = \
+        "Probably because of the instruction pipelining while performing both loops it occurs, that empty one executes slower."
     empty_loop_end = time.clock()
+    print("Empty loop time:" + str(empty_loop_end - empty_loop_start))
     loop_start = time.clock()
-    for i in range (-SIZE - 0x1, SIZE + 0x1):
-        this_is_to_avoid_negative_time = "Problem exists probably because of the cycles required to perform both loops and it occurs, that empty one executes slower."
+    for i in range (-SIZE, SIZE):
+        this_is_to_avoid_negative_time = \
+        "Probably because of the instruction pipelining while performing both loops it occurs, that empty one executes slower."
         value = func(i)
     loop_end = time.clock()
+    print("Full time:" + str(loop_end - loop_start))
     return (loop_end - loop_start) - (empty_loop_end - empty_loop_start)
 
 def my_oct(value: int):
@@ -39,18 +43,10 @@ def my_oct(value: int):
         oct_value = character + oct_value
     return (oct_prefix + oct_value)
 
-def test():
-    for i in range (-SIZE - 0x1, SIZE + 0x1):
-        if my_oct(i) != oct(i):
-            raise Exception()
-    if my_oct(EXTREME_VALUE) != oct(EXTREME_VALUE):
-            raise Exception()
-
-
 if __name__ == "__main__":
     generate_test_data()
-    test()
-    print(measure(oct))
-    print(measure(my_oct))
+    for i in range(0, 3):
+        print("Oct summed execution time:" + str(measure(oct)))
+        print("My_oct summed func execution time:" + str(measure(my_oct)))
     input()
 
